6f37308d52c9f9328f472839f2d65f2628b2d00e
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-08-05 23:04:43 +0300

    editing login/register

--- contact-manager-frontend/src/pages/register/index.jsx
+++ contact-manager-frontend/src/pages/register/index.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 
-
-
 function Register() {
     return (
         <div class="container">
@@ -15,53 +13,28 @@ function Register() {
                 <div class="user-info">
 
                     <div class="label-input">
-
-                        <label for="firstname">First Name </label>
-                        <input id="firstname" name="firstname" type="text" required placeholder="First Name" tabindex="10"/>
+                        <label for="name">Name </label>
+                        <input id="name" name="name" type="text" required placeholder="Name" tabindex="10"/>
                     </div>
-                    <div class="label-input">
 
-                        <label for="lastname">Last Name </label>
-                        <input id="lastname" name="lastname" type="text" required placeholder="Last Name" tabindex="10"/>
-                    </div>
                     <div class="label-input">
-
                         <label for="email">Email </label>
                         <input id="email" name="email" type="email" required placeholder="Email" tabindex="10"/>
                     </div>
-                    <div class="label-input">
 
+                    <div class="label-input">
                         <label for="password">Password </label>
                         <input id="password" name="password" type="password" required placeholder="Password"/>
                     </div>
+
                     <div class="label-input">
-
                         <label for="check-password">Confirm Pass </label>
-                        <input id="check-password" name="check-password" type="password" required
-                            placeholder="Confirm Password"/>
-                    </div>
-                </div>
-
-                <div class="user-type">
-                    <h4>Account Type : </h4>
-                    <div class="types">
-                        <div class="label-radio">
-
-                            <input required type="radio" name="type" value="user"/>
-                            <label for="User">User</label>
-                        </div>
-                        <div class="label-radio">
-
-                            <input required type="radio" name="type" value="admin"/>
-                            <label for="Admin">Admin</label>
-                        </div>
-
+                        <input id="check-password" name="check-password" type="password" required placeholder="Confirm Password"/>
                     </div>
                 </div>
 
                 <button type="submit" id="sign-up">Sign Up</button>
             </form>
-
 
             <div class="bottom-form">
                 <p>Already have an account?</p>
