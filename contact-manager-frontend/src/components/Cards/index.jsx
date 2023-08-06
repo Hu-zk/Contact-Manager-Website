30fdc27e5af7d7a432ecad0f7d9e4858dea15b39
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-08-06 22:56:08 +0300

    styling contacts

--- contact-manager-frontend/src/components/Cards/index.jsx
+++ contact-manager-frontend/src/components/Cards/index.jsx
@@ -4,11 +4,11 @@ function Cards({contacts}) {
 
     return (
         <div className="table-container">
-            <div id="product-list" className="photo-and-desc">
+            <div className="photo-and-desc">
 
                 {contacts.map((contacts,index)=>(
                     <div className="back" key={index}>
-                        <ul className="product_description_list"><b>{contacts.name}</b>
+                        <ul className="contact_description_list"><b>{contacts.name}</b>
                             <li>Phone: {contacts.phone}</li>
                             <li>Address: {contacts.latitude},{contacts.longitude}</li>
                         </ul>
